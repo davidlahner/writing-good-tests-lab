034edec0e7fa87ebbccd33cf2fb11db740ba7907
David Eggerschwiler <mister_x4@hotmail.com>
2019-01-15 13:59:45 +0100

    Disable failing test which is a starting point for the exercise.

--- legacy-code/src/test/java/com/gildedrose/GildedRoseTest.java
+++ legacy-code/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,19 @@ package com.gildedrose;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class GildedRoseTest {
 
+    @Disabled
+    //Take this test as a starting point
     @Test
     public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        assertEquals("fixme", app.items[0].name);
     }
 
 }
