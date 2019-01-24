68168b2752894c53bb22db003b3f112a6f038ffa
David Eggerschwiler <mister_x4@hotmail.com>
2019-01-24 14:51:30 +0100

    Code formatted

--- extensions-rules/src/test/java/com/zuehlke/testing/rules/solutions/CompanyDaoTest.java
+++ extensions-rules/src/test/java/com/zuehlke/testing/rules/solutions/CompanyDaoTest.java
@@ -1,54 +1,54 @@
 package com.zuehlke.testing.rules.solutions;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
-
-import java.util.Collection;
-import java.util.LinkedList;
-
+import com.zuehlke.testing.rules.Company;
+import com.zuehlke.testing.rules.CompanyDao;
 import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.junit.rules.ExternalResource;
 
-import com.zuehlke.testing.rules.Company;
-import com.zuehlke.testing.rules.CompanyDao;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class CompanyDaoTest {
 
-	private CompanyDao dao = new CompanyDao();
+    private CompanyDao dao = new CompanyDao();
 
-	@Rule
-	public CompanyResource companyResource = new CompanyResource();
+    @Rule
+    public CompanyResource companyResource = new CompanyResource();
 
-	@Test
-	public void find() {
-		// arrange
-		Company expected = companyResource.createCompany("Z�hlke Engineering AG");
-		// act
-		Company result = dao.find(expected.getName());
-		// assert
-		assertThat(result.getName(), is(equalTo(expected.getName())));
-	}
+    @Test
+    public void find() {
+        // arrange
+        Company expected = companyResource.createCompany("Zuehlke Engineering AG");
+        // act
+        Company result = dao.find(expected.getName());
+        // assert
+        assertThat(result.getName(), is(equalTo(expected.getName())));
+    }
 
-	private class CompanyResource extends ExternalResource {
+    private class CompanyResource extends ExternalResource {
 
-		private CompanyDao dao = new CompanyDao();
-		private Collection<Company> companies = new LinkedList<>();
+        private CompanyDao dao = new CompanyDao();
+        private Collection<Company> companies = new LinkedList<>();
 
-		public Company createCompany(String name) {
-			final Company company = new Company(name);
-			dao.save(company);
-			companies.add(company);
-			return company;
-		}
+        public Company createCompany(String name) {
+            final Company company = new Company(name);
+            dao.save(company);
+            companies.add(company);
+            return company;
+        }
 
-		@Override
-		protected void after() {
-			for (Company company : companies) {
-				dao.delete(company);
-			}
-			super.after();
-		}
+        @Override
+        protected void after() {
+            for (Company company : companies) {
+                dao.delete(company);
+            }
+            super.after();
+        }
 
-	}
+    }
 }
--- extensions-rules/src/test/java/com/zuehlke/testing/rules/solutions/ExceptionThrowerTest.java
+++ extensions-rules/src/test/java/com/zuehlke/testing/rules/solutions/ExceptionThrowerTest.java
@@ -1,89 +1,83 @@
 package com.zuehlke.testing.rules.solutions;
 
+import com.zuehlke.testing.rules.exercises.ExceptionThrower;
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.hamcrest.Description;
-import org.hamcrest.TypeSafeMatcher;
-import org.junit.Rule;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.rules.ExpectedException;
-
-import com.zuehlke.testing.rules.exercises.ExceptionThrower;
-
 @ExtendWith(LogTestExecutionExtension.class)
-public class ExceptionThrowerTest {
+class ExceptionThrowerTest {
 
-	private ExceptionThrower testee = new ExceptionThrower();
+    private ExceptionThrower testee = new ExceptionThrower();
 
-	@Rule
-	public ExpectedException thrown = ExpectedException.none();
+    @Test
+    void testThrowRuntimeException_illegalParameter_illegalArgumentMessage() {
+        // act
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> testee.throwRuntimeException(-1));
 
-	@Test
-	public void testThrowRuntimeException_illegalParameter_illegalArgumentMessage() {
-		// act
-		RuntimeException exception = assertThrows(RuntimeException.class, () -> testee.throwRuntimeException(-1));
+        //assert
+        assertThat(exception.getMessage(), startsWith("Illegal argument"));
+    }
 
-		//assert
-		assertThat(exception.getMessage(),startsWith("Illegal argument"));
-	}
+    @Test
+    void testThrowRuntimeException_legalParameter_runtimeMessage() {
+        // act
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> testee.throwRuntimeException(1));
 
-	@Test
-	public void testThrowRuntimeException_legalParameter_runtimeMessage() {
-		// act
-		RuntimeException exception = assertThrows(RuntimeException.class, () -> testee.throwRuntimeException(1));
+        //assert
+        assertThat(exception.getMessage(), is(equalTo("Runtime exception occurred")));
+    }
 
-		//assert
-		assertThat(exception.getMessage(),is(equalTo("Runtime exception occurred")));
-	}
+    @Test
+    void testThrowExceptionWithCause_expectCauseType() throws Exception {
+        // act
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> testee.throwExceptionWithCause());
 
-	@Test
-	public void testThrowExceptionWithCause_expectCauseType() throws Exception {
-		// act
-		RuntimeException exception = assertThrows(RuntimeException.class, () -> testee.throwExceptionWithCause());
+        //assert
+        assertThat(exception.getMessage(), is(equalTo("outer exception")));
+        assertThat(exception.getCause(), instanceOf(NullPointerException.class));
+    }
 
-		//assert
-		assertThat(exception.getMessage(),is(equalTo("outer exception")));
-		assertThat(exception.getCause(), instanceOf(NullPointerException.class));
-	}
+    @Test
+    void testThrowExceptionWithCause_expectCauseWithMessage() throws Exception {
+        // act
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> testee.throwExceptionWithCause());
 
-	@Test
-	public void testThrowExceptionWithCause_expectCauseWithMessage() throws Exception {
-		// act
-		RuntimeException exception = assertThrows(RuntimeException.class, () -> testee.throwExceptionWithCause());
+        //assert
+        assertThat(exception.getMessage(), is(equalTo("outer exception")));
+        assertThat(exception.getCause(), CauseMatcher.matchesCause(NullPointerException.class,
+                "Oops! Something wasn't supposed to be null here."));
+    }
 
-		//assert
-		assertThat(exception.getMessage(),is(equalTo("outer exception")));
-		assertThat(exception.getCause(), CauseMatcher.matchesCause(NullPointerException.class,
-				"Oops! Something wasn't supposed to be null here."));
-	}
+    private static class CauseMatcher extends TypeSafeMatcher<Throwable> {
 
-	private static class CauseMatcher extends TypeSafeMatcher<Throwable> {
+        private final Class<? extends Throwable> type;
+        private final String expectedMessage;
 
-		private final Class<? extends Throwable> type;
-		private final String expectedMessage;
+        private CauseMatcher(Class<? extends Throwable> type, String expectedMessage) {
+            this.type = type;
+            this.expectedMessage = expectedMessage;
+        }
 
-		private CauseMatcher(Class<? extends Throwable> type, String expectedMessage) {
-			this.type = type;
-			this.expectedMessage = expectedMessage;
-		}
+        @Override
+        protected boolean matchesSafely(Throwable item) {
+            return item.getClass().isAssignableFrom(type) && item.getMessage().contains(expectedMessage);
+        }
 
-		@Override
-		protected boolean matchesSafely(Throwable item) {
-			return item.getClass().isAssignableFrom(type) && item.getMessage().contains(expectedMessage);
-		}
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("expects type ").appendValue(type).appendText(" and a message ")
+                    .appendValue(expectedMessage);
+        }
 
-		@Override
-		public void describeTo(Description description) {
-			description.appendText("expects type ").appendValue(type).appendText(" and a message ")
-					.appendValue(expectedMessage);
-		}
-
-		public static CauseMatcher matchesCause(Class<? extends Throwable> type, String expectedMessage) {
-			return new CauseMatcher(type, expectedMessage);
-		}
-	}
+        public static CauseMatcher matchesCause(Class<? extends Throwable> type, String expectedMessage) {
+            return new CauseMatcher(type, expectedMessage);
+        }
+    }
 
 }
