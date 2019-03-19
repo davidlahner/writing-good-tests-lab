0b8b381038d7e93fc95731885ab7147030e60fef
David Eggerschwiler <mister_x4@hotmail.com>
2019-03-19 07:31:19 +0100

    remove unused code.

--- test-driven-development/src/test/java/com/zuehlke/testing/tdd/solutions/CalculatorTest.java
+++ test-driven-development/src/test/java/com/zuehlke/testing/tdd/solutions/CalculatorTest.java
@@ -1,54 +1,50 @@
 package com.zuehlke.testing.tdd.solutions;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.Rule;
-import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
+class CalculatorTest {
+    private Calculator testee = new Calculator();
 
-public class CalculatorTest {
-	@Rule
-	public ExpectedException thrown = ExpectedException.none();
+    @Test
+    void testCalculate_emptyString_0() {
+        // act
+        int sum = testee.calculate("");
+        // assert
+        assertThat(sum, is(equalTo(0)));
+    }
 
-	private Calculator testee = new Calculator();
+    @Test
+    void testCalculate_oneNumber_number() {
+        // act
+        int sum = testee.calculate("1");
+        // assert
+        assertThat(sum, is(equalTo(1)));
+    }
 
-	@Test
-	public void testCalculate_emptyString_0() {
-		// act
-		int sum = testee.calculate("");
-		// assert
-		assertThat(sum, is(equalTo(0)));
-	}
+    @Test
+    void testCalculate_twoNumbers_sum() {
+        // act
+        int sum = testee.calculate("1,2");
+        // assert
+        assertThat(sum, is(equalTo(3)));
+    }
 
-	@Test
-	public void testCalculate_oneNumber_number() {
-		// act
-		int sum = testee.calculate("1");
-		// assert
-		assertThat(sum, is(equalTo(1)));
-	}
+    @Test
+    void testCalculate_threeNumbers_sum() {
+        // act
+        int sum = testee.calculate("1,2,5");
+        // assert
+        assertThat(sum, is(equalTo(8)));
+    }
 
-	@Test
-	public void testCalculate_twoNumbers_sum() {
-		// act
-		int sum = testee.calculate("1,2");
-		// assert
-		assertThat(sum, is(equalTo(3)));
-	}
-
-	@Test
-	public void testCalculate_threeNumbers_sum() {
-		// act
-		int sum = testee.calculate("1,2,5");
-		// assert
-		assertThat(sum, is(equalTo(8)));
-	}
-
-	@Test
-	public void testCalculate_fourNumbers_exception() {
-		// act
-		assertThrows(IllegalArgumentException.class, () -> testee.calculate("1,2,5,6"));
-	}
+    @Test
+    void testCalculate_fourNumbers_exception() {
+        // act
+        assertThrows(IllegalArgumentException.class, () -> testee.calculate("1,2,5,6"));
+    }
 }
