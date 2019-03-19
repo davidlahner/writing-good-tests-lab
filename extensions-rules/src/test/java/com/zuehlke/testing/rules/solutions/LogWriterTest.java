5020760d27c12ac6e5224f8cb4d1fb5d06c74414
David Eggerschwiler <mister_x4@hotmail.com>
2019-03-19 07:31:07 +0100

    Use new @TempDir instead of TemporaryFolderRule.

--- extensions-rules/src/test/java/com/zuehlke/testing/rules/solutions/LogWriterTest.java
+++ extensions-rules/src/test/java/com/zuehlke/testing/rules/solutions/LogWriterTest.java
@@ -1,35 +1,30 @@
 package com.zuehlke.testing.rules.solutions;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
+import com.zuehlke.testing.rules.exercises.LogWriter;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
-import org.junit.Rule;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.migrationsupport.rules.EnableRuleMigrationSupport;
-import org.junit.rules.TemporaryFolder;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 
-import com.zuehlke.testing.rules.exercises.LogWriter;
+class LogWriterTest {
 
-@EnableRuleMigrationSupport
-public class LogWriterTest {
+    private LogWriter testee = new LogWriter();
 
-	private LogWriter testee = new LogWriter();
-
-	@Rule
-	public TemporaryFolder tempFolder = new TemporaryFolder();
-
-	@Test
-	public void log() throws IOException {
-		// arrange
-		File logFile = tempFolder.newFile("logFile.txt");
-		// act
-		testee.log(logFile, "Hello Log");
-		// assert
-		String result = new String(Files.readAllBytes(logFile.toPath()));
-		assertThat(result, containsString("Hello Log"));
-	}
+    @Test
+    void log(@TempDir Path tempDir) throws IOException {
+        // arrange
+        Path filePath = tempDir.resolve("logFile.txt");
+        File logFile = new File(filePath.toUri());
+        // act
+        testee.log(logFile, "Hello Log");
+        // assert
+        String result = new String(Files.readAllBytes(logFile.toPath()));
+        assertThat(result, containsString("Hello Log"));
+    }
 }
