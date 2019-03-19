3d2ed88b1f1406334c8a98743b57db243dc18222
David Eggerschwiler <mister_x4@hotmail.com>
2019-03-19 07:15:44 +0100

    Remove unused code

--- extensions-rules/src/test/java/com/zuehlke/testing/rules/examples/TimeoutExample.java
+++ extensions-rules/src/test/java/com/zuehlke/testing/rules/examples/TimeoutExample.java
@@ -1,64 +1,58 @@
 package com.zuehlke.testing.rules.examples;
 
-import static org.hamcrest.Matchers.*;
-import static org.hamcrest.MatcherAssert.*;
-import static org.junit.jupiter.api.Assertions.assertTimeout;
-import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
-
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
+import com.zuehlke.testing.rules.Person;
+import com.zuehlke.testing.rules.PersonDao;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.Rule;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.junit.rules.Timeout;
 
-import com.zuehlke.testing.rules.Person;
-import com.zuehlke.testing.rules.PersonDao;
+import java.time.Duration;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
 @Disabled
 //TODO: remove @Disabled to run test
-public class TimeoutExample {
+class TimeoutExample {
 
-	private PersonDao dao = new PersonDao();
+    private PersonDao dao = new PersonDao();
 
-	@RegisterExtension
-	public PersonResource personResource = new PersonResource();
+    @RegisterExtension
+    private PersonResource personResource = new PersonResource();
 
-	@Rule
-	public Timeout globalTimeout = new Timeout(2, TimeUnit.SECONDS);
+    private Person expected;
 
-	private Person expected;
+    @BeforeEach
+    void init() {
+        expected = personResource.createPerson("Test");
+    }
 
-	@BeforeEach
-	public void init() {
-		expected = personResource.createPerson("Test");
-	}
+    @Test
+    void fastQuery_withoutTimeout() {
+        // act
+        Person result = dao.find("Test");
+        // assert
+        assertThat(result, is(equalTo(expected)));
+    }
 
-	@Test
-	public void fastQuery_withoutTimeout() {
-		// act
-		Person result = dao.find("Test");
-		// assert
-		assertThat(result, is(equalTo(expected)));
-	}
-
-	@Test
-	public void complexQuery_withTimeout() {
-		// act
-		Person result = assertTimeout(Duration.ofSeconds(2), () -> dao.longLastingQuery("Test"));
-		// assert
-		assertThat(result, is(equalTo(expected)));
-	}
+    @Test
+    void complexQuery_withTimeout() {
+        // act
+        Person result = assertTimeout(Duration.ofSeconds(2), () -> dao.longLastingQuery("Test"));
+        // assert
+        assertThat(result, is(equalTo(expected)));
+    }
 
 
-	@Test
-	public void complexQuery_withPreemptiveTimeout() {
-		// act
-		Person result = assertTimeoutPreemptively(Duration.ofSeconds(2), () -> dao.longLastingQuery("Test"));
-		// assert
-		assertThat(result, is(equalTo(expected)));
-	}
+    @Test
+    void complexQuery_withPreemptiveTimeout() {
+        // act
+        Person result = assertTimeoutPreemptively(Duration.ofSeconds(2), () -> dao.longLastingQuery("Test"));
+        // assert
+        assertThat(result, is(equalTo(expected)));
+    }
 }
