28b746dc87daf1a18d97dfaf7ae30b34116e5f57
David Eggerschwiler <mister_x4@hotmail.com>
2019-04-04 07:32:26 +0200

    Move JUnit 4 dependency to sub-projects that actually use it. Remove unused imports.

--- testdata/src/test/java/com/zuehlke/testing/testdata/examples/PrimeFactorTest.java
+++ testdata/src/test/java/com/zuehlke/testing/testdata/examples/PrimeFactorTest.java
@@ -1,55 +1,49 @@
 package com.zuehlke.testing.testdata.examples;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Stream;
-
-import org.junit.jupiter.api.Test;
+import com.zuehlke.testing.testdata.PrimeFactor;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
 
-import com.zuehlke.testing.testdata.PrimeFactor;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class PrimeFactorTest {
 
-	@ParameterizedTest
-	@MethodSource("argumentsProvider")
-	void primeFactors(int product, List<Integer> factors) {
-		// act
-		List<Integer> result = PrimeFactor.factorsOf(product);
-		// arrange
-		assertThat(result, is(equalTo(factors)));
-	}
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    void primeFactors(int product, List<Integer> factors) {
+        // act
+        List<Integer> result = PrimeFactor.factorsOf(product);
+        // arrange
+        assertThat(result, is(equalTo(factors)));
+    }
 
-	static Stream<Arguments> argumentsProvider() {
-		return Stream.of(
-				arguments( 1, Arrays.asList() ), //
-				arguments( 2, Arrays.asList(2) ), //
-				arguments( 3, Arrays.asList(3) ), //
-				arguments( 4, Arrays.asList(2, 2) ), //
-				arguments( 5, Arrays.asList(5) ), //
-				arguments( 6, Arrays.asList(2, 3) ), //
-				arguments( 7, Arrays.asList(7) ), //
-				arguments( 8, Arrays.asList(2, 2, 2) ), //
-				arguments( 9, Arrays.asList(3, 3) ), //
-				arguments( 10, Arrays.asList(2, 5) ), //
-				arguments( 12, Arrays.asList(2, 2, 3) ), //
-				arguments( 16, Arrays.asList(2, 2, 2, 2) ), //
-				arguments( 18, Arrays.asList(2, 3, 3) ), //
-				arguments( 26, Arrays.asList(2, 13) ), //
-				arguments( 99, Arrays.asList(3, 3, 11) ), //
-				arguments( 101, Arrays.asList(101) ) //
-		);
-	}
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                arguments(1, Arrays.asList()), //
+                arguments(2, Arrays.asList(2)), //
+                arguments(3, Arrays.asList(3)), //
+                arguments(4, Arrays.asList(2, 2)), //
+                arguments(5, Arrays.asList(5)), //
+                arguments(6, Arrays.asList(2, 3)), //
+                arguments(7, Arrays.asList(7)), //
+                arguments(8, Arrays.asList(2, 2, 2)), //
+                arguments(9, Arrays.asList(3, 3)), //
+                arguments(10, Arrays.asList(2, 5)), //
+                arguments(12, Arrays.asList(2, 2, 3)), //
+                arguments(16, Arrays.asList(2, 2, 2, 2)), //
+                arguments(18, Arrays.asList(2, 3, 3)), //
+                arguments(26, Arrays.asList(2, 13)), //
+                arguments(99, Arrays.asList(3, 3, 11)), //
+                arguments(101, Arrays.asList(101)) //
+        );
+    }
 
 }
--- testdata/src/test/java/com/zuehlke/testing/testdata/solutions/PrimeFactorPrimeTest.java
+++ testdata/src/test/java/com/zuehlke/testing/testdata/solutions/PrimeFactorPrimeTest.java
@@ -1,64 +1,56 @@
 package com.zuehlke.testing.testdata.solutions;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.stream.Stream;
-
+import com.zuehlke.testing.testdata.PrimeFactor;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
 
-import com.zuehlke.testing.testdata.PrimeFactor;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class PrimeFactorPrimeTest {
 
-	@DisplayName("is Prime with CsvSource")
-	@ParameterizedTest(name = "result for \"{0}\" should be {1}")
-	@CsvSource({"1, false ", "2, true ", "3, true ","4, false ", "5, true ", "6, false ", "7, true ", "8, false ", //
-			"26, false ", "99, false ", "101, true ", "102, false "})
-	void isPrimeCsvSource(int number, boolean primeExpected) {
-		// act
-		boolean result = PrimeFactor.isPrime(number);
-		// assert
-		assertThat(result, is(primeExpected));
-	}
+    @DisplayName("is Prime with CsvSource")
+    @ParameterizedTest(name = "result for \"{0}\" should be {1}")
+    @CsvSource({"1, false ", "2, true ", "3, true ", "4, false ", "5, true ", "6, false ", "7, true ", "8, false ", //
+            "26, false ", "99, false ", "101, true ", "102, false "})
+    void isPrimeCsvSource(int number, boolean primeExpected) {
+        // act
+        boolean result = PrimeFactor.isPrime(number);
+        // assert
+        assertThat(result, is(primeExpected));
+    }
 
-	@DisplayName("is Prime with MethodSource")
-	@ParameterizedTest(name = "result for \"{0}\" should be {1}")
-	@MethodSource("argumentsProvider")
-	void isPrimeMethodSource(int number, boolean primeExpected) {
-		// act
-		boolean result = PrimeFactor.isPrime(number);
-		// assert
-		assertThat(result, is(primeExpected));
-	}
+    @DisplayName("is Prime with MethodSource")
+    @ParameterizedTest(name = "result for \"{0}\" should be {1}")
+    @MethodSource("argumentsProvider")
+    void isPrimeMethodSource(int number, boolean primeExpected) {
+        // act
+        boolean result = PrimeFactor.isPrime(number);
+        // assert
+        assertThat(result, is(primeExpected));
+    }
 
-	static Stream<Arguments> argumentsProvider() {
-		return Stream.of(
-				arguments( 1, false ), //
-				arguments( 2, true ), //
-				arguments( 3, true ), //
-				arguments( 4, false ), //
-				arguments( 5, true ), //
-				arguments( 6, false ), //
-				arguments( 7, true ), //
-				arguments( 8, false ), //
-				arguments( 26, false ), //
-				arguments( 99, false ), //
-				arguments( 101, true ), //
-				arguments( 102, false ) //
-		);
-	}
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                arguments(1, false), //
+                arguments(2, true), //
+                arguments(3, true), //
+                arguments(4, false), //
+                arguments(5, true), //
+                arguments(6, false), //
+                arguments(7, true), //
+                arguments(8, false), //
+                arguments(26, false), //
+                arguments(99, false), //
+                arguments(101, true), //
+                arguments(102, false) //
+        );
+    }
 
 }
