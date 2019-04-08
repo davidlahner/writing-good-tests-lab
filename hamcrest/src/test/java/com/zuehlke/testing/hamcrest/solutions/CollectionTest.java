5f160373e45f72ff9fc3314271da8ca365ba8a66
David Eggerschwiler <mister_x4@hotmail.com>
2019-04-08 13:23:12 +0300

    Add solution for hasItem with object which does not implement equals.

--- hamcrest/src/test/java/com/zuehlke/testing/hamcrest/solutions/CollectionTest.java
+++ hamcrest/src/test/java/com/zuehlke/testing/hamcrest/solutions/CollectionTest.java
@@ -1,47 +1,45 @@
 package com.zuehlke.testing.hamcrest.solutions;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
+import com.zuehlke.testing.hamcrest.Person;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
-import com.zuehlke.testing.hamcrest.Person;
+class CollectionTest {
 
-public class CollectionTest {
+    @Test
+    void contains_primitives() {
+        // arrange
+        List<Integer> values = Arrays.asList(1, 3, 4, 2, 5);
 
-	@Test
-	public void contains_primitives() {
-		// arrange
-		List<Integer> values = Arrays.asList(1, 3, 4, 2, 5);
+        // assert
+        // 1. assert 3 is present
+        assertThat(values, hasItem(3));
+        // 2. assert 1,3,4,2,5 are in the list
+        assertThat(values, contains(1, 3, 4, 2, 5));
+        // 3. assert 1,2,3,4,5 are in the list
+        assertThat(values, containsInAnyOrder(1, 2, 3, 4, 5));
+    }
 
-		// assert
-		// 1. assert 3 is present
-		assertThat(values, hasItem(3));
-		// 2. assert 1,3,4,2,5 are in the list
-		assertThat(values, contains(1, 3, 4, 2, 5));
-		// 3. assert 1,2,3,4,5 are in the list
-		assertThat(values, containsInAnyOrder(1, 2, 3, 4, 5));
-	}
+    @Test
+    void contains_objects() {
+        // arrange
+        Person peter = new Person(1, "Peter", "Maler", 42);
+        Person fritz = new Person(2, "Fritz", "Meier", 5);
+        List<Person> values = Arrays.asList(peter, fritz);
 
-	@Test
-	public void contains_objects() {
-		// arrange
-		Person peter = new Person(1, "Peter", "Maler", 42);
-		Person fritz = new Person(2, "Fritz", "Meier", 5);
-		List<Person> values = Arrays.asList(peter, fritz);
-
-		// assert
-		// 1. assert fritz is present
-		assertThat(values, hasItem(fritz));
-		// 2. assert new Person(2, "Fritz", "Meier", 5) is present
-		// For the following line to work, Person needs to implement equals()
-		// assertThat(values, hasItem(new Person(2, "Fritz", "Meier", 5)));
-		// 3. assert peter, fritz are in the list
-		assertThat(values, contains(peter, fritz));
-		// 4. assert fritz, peter are in the list
-		assertThat(values, containsInAnyOrder(fritz, peter));
-	}
+        // assert
+        // 1. assert fritz is present
+        assertThat(values, hasItem(fritz));
+        // 2. assert new Person(2, "Fritz", "Meier", 5) is present
+        assertThat(values, hasItem(samePropertyValuesAs(new Person(2, "Fritz", "Meier", 5))));
+        // 3. assert peter, fritz are in the list
+        assertThat(values, contains(peter, fritz));
+        // 4. assert fritz, peter are in the list
+        assertThat(values, containsInAnyOrder(fritz, peter));
+    }
 }
