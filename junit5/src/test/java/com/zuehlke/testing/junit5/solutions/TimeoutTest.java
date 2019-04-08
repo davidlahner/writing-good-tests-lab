ce83ba8b2a6c0dcc091c9a85546889a30c15b162
David Eggerschwiler <mister_x4@hotmail.com>
2019-04-08 13:28:19 +0300

    Added additional example explaining why Duration.ZERO might not fail in every case.

--- junit5/src/test/java/com/zuehlke/testing/junit5/solutions/TimeoutTest.java
+++ junit5/src/test/java/com/zuehlke/testing/junit5/solutions/TimeoutTest.java
@@ -15,4 +15,16 @@ class TimeoutTest {
             Thread.sleep(1002);
         });
     }
+
+    /**
+     * assertTimeout measures time in milliseconds. So if we set the timeout to Duration.ZERO
+     * and execute code that is faster than 1 millisecond then the test will pass.
+     */
+    @Test
+    void testTimeout_withDurationZero() {
+        assertTimeout(Duration.ZERO, () -> {
+            /* do something */
+            Thread.sleep(1);
+        });
+    }
 }
