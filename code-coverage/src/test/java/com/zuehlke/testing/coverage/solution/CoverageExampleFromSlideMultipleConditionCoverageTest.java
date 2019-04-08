f409ee30746bda2126efbd03c9b6f9595481c9b6
David Eggerschwiler <mister_x4@hotmail.com>
2019-04-08 13:23:54 +0300

    Solutions were mixed up. Updated them to represent correct coverage type.

--- code-coverage/src/test/java/com/zuehlke/testing/coverage/solution/CoverageExampleFromSlideMultipleConditionCoverageTest.java
+++ code-coverage/src/test/java/com/zuehlke/testing/coverage/solution/CoverageExampleFromSlideMultipleConditionCoverageTest.java
@@ -1,34 +1,50 @@
 package com.zuehlke.testing.coverage.solution;
 
-import static org.hamcrest.Matchers.*;
-import static org.hamcrest.MatcherAssert.*;
+import com.zuehlke.testing.coverage.example.CoverageExampleFromSlide;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalTime;
 
-import org.junit.jupiter.api.Test;
-
-import com.zuehlke.testing.coverage.example.CoverageExampleFromSlide;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 class CoverageExampleFromSlideMultipleConditionCoverageTest {
 
-	private CoverageExampleFromSlide testee = new CoverageExampleFromSlide();
+    private CoverageExampleFromSlide testee = new CoverageExampleFromSlide();
 
-	@Test
-	void testIsShopOpen_vip_open() {
-		// act
-		boolean result = testee.isShopOpen(LocalTime.of(9, 00), true);
+    @Test
+    void testIsShopOpen_9amVip_open() {
+        // act
+        boolean result = testee.isShopOpen(LocalTime.of(9, 00), true);
 
-		// assert
-		assertThat(result, is(true));
-	}
+        // assert
+        assertThat(result, is(true));
+    }
 
-	@Test
-	void testIsShopOpen_8am_notOpen() {
-		// act
-		boolean result = testee.isShopOpen(LocalTime.of(8, 00), false);
+    @Test
+    void testIsShopOpen_8amNotVip_notOpen() {
+        // act
+        boolean result = testee.isShopOpen(LocalTime.of(8, 00), false);
 
-		// assert
-		assertThat(result, is(false));
-	}
+        // assert
+        assertThat(result, is(false));
+    }
 
+    @Test
+    void testIsShopOpen_8amVip_open() {
+        // act
+        boolean result = testee.isShopOpen(LocalTime.of(8, 00), true);
+
+        // assert
+        assertThat(result, is(true));
+    }
+
+    @Test
+    void testIsShopOpen_7pmNotVip_notOpen() {
+        // act
+        boolean result = testee.isShopOpen(LocalTime.of(19, 00), false);
+
+        // assert
+        assertThat(result, is(false));
+    }
 }
