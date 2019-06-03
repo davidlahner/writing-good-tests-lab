ca4fd5502b6f859ab5c1d39e2712cd6587d299f5
David Eggerschwiler <mister_x4@hotmail.com>
2019-06-03 10:00:01 +0200

    Add an additional test.

--- code-coverage/src/test/java/com/zuehlke/testing/coverage/vendingmachine/solution/VendingMachineTest.java
+++ code-coverage/src/test/java/com/zuehlke/testing/coverage/vendingmachine/solution/VendingMachineTest.java
@@ -1,94 +1,103 @@
 package com.zuehlke.testing.coverage.vendingmachine.solution;
 
-import static org.hamcrest.Matchers.*;
-import static org.hamcrest.MatcherAssert.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.Rule;
-import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
-
 import com.zuehlke.testing.coverage.vendingmachine.NoMoreChangeException;
 import com.zuehlke.testing.coverage.vendingmachine.NotEnoughMoneyException;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class VendingMachineTest {
 
-	@Test
-	void calculateChange_orderFiveBottlesWithoutEnoughMoney_throwsNotEnoughMoneyException() {
-		// arrange
-		VendingMachine testee = new VendingMachine(100, 5, 10);
+    @Test
+    void calculateChange_orderFiveBottlesWithoutEnoughMoney_throwsNotEnoughMoneyException() {
+        // arrange
+        VendingMachine testee = new VendingMachine(100, 5, 10);
 
-		// act
-		NotEnoughMoneyException exception = assertThrows(NotEnoughMoneyException.class, () -> testee.calculateChange(1, 4));
+        // act
+        NotEnoughMoneyException exception = assertThrows(NotEnoughMoneyException.class, () -> testee.calculateChange(1, 4));
 
-		//assert
-		assertThat(exception.getMessage(), is(equalTo("Not enough money: Paid: 4, required: 5")));
-	}
+        //assert
+        assertThat(exception.getMessage(), is(equalTo("Not enough money: Paid: 4, required: 5")));
+    }
 
-	@Test
-	void calculateChange_orderOneBottleWithExactChange_returns0Change()
-			throws NoMoreChangeException, NotEnoughMoneyException {
-		// arrange
-		VendingMachine testee = new VendingMachine(100, 5, 10);
+    //No Change returned
+    @Test
+    void calculateChange_orderOneBottleWithExactChange_returns0Change()
+            throws NoMoreChangeException, NotEnoughMoneyException {
+        // arrange
+        VendingMachine testee = new VendingMachine(100, 5, 10);
+        // act
+        int change = testee.calculateChange(1, 5);
+        // assert
+        assertThat(change, is(equalTo(0)));
+    }
 
-		// act
-		int change = testee.calculateChange(1, 5);
+    //Some Change returned
+    @Test
+    void calculateChange_orderOneBottleOverpaying_returnsChange()
+            throws NoMoreChangeException, NotEnoughMoneyException {
+        // arrange
+        VendingMachine testee = new VendingMachine(100, 5, 10);
+        // act
+        int change = testee.calculateChange(1, 6);
+        // assert
+        assertThat(change, is(equalTo(1)));
+    }
 
-		// assert
-		assertThat(change, is(equalTo(0)));
-	}
+    @Test
+    void calculateChange_orderOneBottlePayingTooMuchMachineDoesntHaveChange_throwsNoMoreChangeException() {
+        // arrange
+        VendingMachine testee = new VendingMachine(1, 5, 10);
+        // act
+        NoMoreChangeException exception = assertThrows(NoMoreChangeException.class,
+                () -> testee.calculateChange(1, 7));
+        //assert
+        assertThat(exception.getMessage(),
+                is(equalTo("No More Change: Vending machine ran out of 1CHF coins")));
+    }
 
-	@Test
-	void calculateChange_orderOneBottlePayingTooMuchMachineDoesntHaveChange_throwsNoMoreChangeException() {
-		// arrange
-		VendingMachine testee = new VendingMachine(1, 5, 10);
+    @Test
+    void setEachXthBottleForFree_everyBottleFree_getTwoBottlesForFree()
+            throws NoMoreChangeException, NotEnoughMoneyException {
+        // arrange
+        VendingMachine testee = new VendingMachine(100, 5, 10);
 
-		// act
-		NoMoreChangeException exception = assertThrows(NoMoreChangeException.class, () -> testee.calculateChange(1, 7));
+        // act
+        testee.setEachXthBottleForFree(1);
+        int change = testee.calculateChange(2, 0);
 
-		//assert
-		assertThat(exception.getMessage(), is(equalTo("No More Change: Vending machine ran out of 1CHF coins")));
-	}
+        // assert
+        assertThat(change, is(equalTo(0)));
+    }
 
-	@Test
-	void setEachXthBottleForFree_everyBottleFree_getTwoBottlesForFree()
-			throws NoMoreChangeException, NotEnoughMoneyException {
-		// arrange
-		VendingMachine testee = new VendingMachine(100, 5, 10);
+    @Test
+    void reducePricePerBottle_reduceTo1_priceAdjusted_buyTwoBottlesFor2()
+            throws NoMoreChangeException, NotEnoughMoneyException {
+        // arrange
+        VendingMachine testee = new VendingMachine(100, 5, 10);
 
-		// act
-		testee.setEachXthBottleForFree(1);
-		int change = testee.calculateChange(2, 0);
+        // act
+        testee.reducePricePerBottle(4);
+        int change = testee.calculateChange(2, 2);
 
-		// assert
-		assertThat(change, is(equalTo(0)));
-	}
+        // assert
+        assertThat(change, is(equalTo(0)));
+    }
 
-	@Test
-	void reducePricePerBottle_reduceTo1_priceAdjusted_buyTwoBottlesFor2()
-			throws NoMoreChangeException, NotEnoughMoneyException {
-		// arrange
-		VendingMachine testee = new VendingMachine(100, 5, 10);
+    @Test
+    void reducePricePerBottle_reduceTo0_buyTwoBottlesFor0_throwsNotEnoughMoneyException() {
+        // arrange
+        VendingMachine testee = new VendingMachine(100, 5, 10);
 
-		// act
-		testee.reducePricePerBottle(4);
-		int change = testee.calculateChange(2, 2);
+        // act
+        testee.reducePricePerBottle(5);
+        NotEnoughMoneyException exception = assertThrows(NotEnoughMoneyException.class, () -> testee.calculateChange(2, 0));
 
-		// assert
-		assertThat(change, is(equalTo(0)));
-	}
-
-	@Test
-	void reducePricePerBottle_reduceTo0_buyTwoBottlesFor0_throwsNotEnoughMoneyException() {
-		// arrange
-		VendingMachine testee = new VendingMachine(100, 5, 10);
-
-		// act
-		testee.reducePricePerBottle(5);
-		NotEnoughMoneyException exception = assertThrows(NotEnoughMoneyException.class, () -> testee.calculateChange(2, 0));
-
-		//assert
-		assertThat(exception.getMessage(), is(equalTo("Not enough money: Paid: 0, required: 10")));
-	}
+        //assert
+        assertThat(exception.getMessage(), is(equalTo("Not enough money: Paid: 0, required: 10")));
+    }
 
 }
