9d6439fe8c214b05be3796e2a2785358b55b5143
David Eggerschwiler <mister_x4@hotmail.com>
2019-09-24 08:04:14 +0200

    Remove warnings
    (cherry picked from commit 82971f7dea69792bfb69346526ba312feb6c6428)

--- design-for-testability/src/test/java/com/zuehlke/testing/solutions/StaticCallExtractedTest.java
+++ design-for-testability/src/test/java/com/zuehlke/testing/solutions/StaticCallExtractedTest.java
@@ -1,33 +1,32 @@
 package com.zuehlke.testing.solutions;
 
-import static org.hamcrest.Matchers.*;
-import static org.hamcrest.MatcherAssert.*;
+import com.zuehlke.testing.solutions.StaticCallExtracted.Call;
+import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
-import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
-import com.zuehlke.testing.solutions.StaticCallExtracted.Call;
+class StaticCallExtractedTest {
 
-public class StaticCallExtractedTest {
+    private long[] instants;
+    private int now = 0;
+    private Call testCall = new Call() {
+        @Override
+        long instantAsMillis() {
+            return instants[now++];
+        }
+    };
 
-	private long[] instants;
-	private int now = 0;
-	private Call testCall = new Call() {
-		@Override
-		long instantAsMillis() {
-			return instants[now++];
-		}
-	};
-
-	@Test
-	public void recordingDuration() {
-		// arrange
-		instants = new long[] { 9000000000l, 9000015000l };
-		// act
-		testCall.recordMessage();
-		// assert
-		assertThat(testCall.getDuration(), equalTo(Duration.ofSeconds(15)));
-	}
+    @Test
+    void recordingDuration() {
+        // arrange
+        instants = new long[]{9000000000L, 9000015000L};
+        // act
+        testCall.recordMessage();
+        // assert
+        assertThat(testCall.getDuration(), equalTo(Duration.ofSeconds(15)));
+    }
 
 }
