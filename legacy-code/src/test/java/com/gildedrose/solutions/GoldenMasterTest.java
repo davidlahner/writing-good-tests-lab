dcf315518f5cca9ae4f74088de1e41fb67e5e977
David Eggerschwiler <mister_x4@hotmail.com>
2021-11-20 12:17:27 +0100

    Add solution for Golden Master text file

---
+++ legacy-code/src/test/java/com/gildedrose/solutions/GoldenMasterTest.java
@@ -0,0 +1,59 @@
+package com.gildedrose.solutions;
+
+import com.gildedrose.Item;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GoldenMasterTest {
+
+    private static final String LINE_SEPARATOR = "\n";
+
+    @Test
+    void testGoldenMaster() throws IOException, URISyntaxException {
+        //act
+        String result = runScenario(31);
+        //assert
+        String expected = Files.readString(Paths.get(ClassLoader.getSystemResource("GoldenMasterSolution.txt").toURI()));
+        assertThat(result).isEqualTo(expected);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(runScenario(31));
+    }
+
+    private static String runScenario(int days) {
+        StringBuilder builder = new StringBuilder();
+
+        Item[] items = new Item[]{
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                // this conjured item does not work properly yet
+                new Item("Conjured Mana Cake", 3, 6)};
+
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < days; i++) {
+            builder.append("-------- day ").append(i).append(" --------").append(LINE_SEPARATOR);
+            builder.append("name, sellIn, quality").append(LINE_SEPARATOR);
+            for (Item item : items) {
+                builder.append(item).append(LINE_SEPARATOR);
+            }
+            builder.append(LINE_SEPARATOR);
+            app.updateQuality();
+        }
+        return builder.toString();
+    }
+
+}
