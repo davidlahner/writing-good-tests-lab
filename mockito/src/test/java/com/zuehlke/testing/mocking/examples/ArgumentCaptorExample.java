84003a1736cf310fa7b77a60b08de7894d414bd6
David Eggerschwiler <mister_x4@hotmail.com>
2022-06-23 07:30:43 +0200

    Add example with argThat

--- mockito/src/test/java/com/zuehlke/testing/mocking/examples/ArgumentCaptorExample.java
+++ mockito/src/test/java/com/zuehlke/testing/mocking/examples/ArgumentCaptorExample.java
@@ -7,6 +7,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.argThat;
 
 public class ArgumentCaptorExample {
 
@@ -23,5 +24,17 @@ public class ArgumentCaptorExample {
         ArgumentCaptor<String> arg = ArgumentCaptor.forClass(String.class);
         Mockito.verify(listener).documentAdded(arg.capture());
         assertThat(arg.getValue()).startsWith("Another");
+    }
+
+    @Test
+    public void addDocument_withContent_withoutCaptor() {
+        // arrange
+        StoreListener listener = Mockito.mock(StoreListener.class);
+        testee.addListener(listener);
+        // act
+        testee.addDocument("Another Document", new byte[]{1, 2, 3, 4, 5});
+        // assert
+        Mockito.verify(listener).documentAdded(
+                argThat(argument -> argument.startsWith("Another2")));
     }
 }
